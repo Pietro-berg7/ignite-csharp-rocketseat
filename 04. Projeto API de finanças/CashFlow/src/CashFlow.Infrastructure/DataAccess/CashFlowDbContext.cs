94ed9a725df4e97e058cfbf55db73dac3522d6cc
Pietro Bergamaschi <pietrovb25@gmail.com>
2024-08-04 20:36:32 -0300

    feat: add dbcontext

---
+++ 04. Projeto API de finanças/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext: DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Integrated Security=True;";
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
