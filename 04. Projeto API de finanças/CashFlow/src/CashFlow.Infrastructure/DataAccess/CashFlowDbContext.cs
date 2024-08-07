70e9bbe77ae314774a55335895eae067a7a6483a
Pietro Bergamaschi <pietrovb25@gmail.com>
2024-08-07 01:21:00 -0300

    feat: configure connection string in appsettings

--- 04. Projeto API de finanças/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ 04. Projeto API de finanças/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,12 +5,7 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext: DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
-
-    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflowdb;Integrated Security=True;TrustServerCertificate=True;";
-
-        optionsBuilder.UseSqlServer(connectionString);
-    }
 }
